e091926bc66be1a5045cb0822e45e5463acaffd1
Paulo mujurujunior <paulo@greystonetec.co.za>
2021-03-15 18:52:32 +0200

    example code for cli

---
+++ examples/cli-basics/os-args/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	// if commnd are less than 2(fmt.Println("no command provided"))
+	if len(os.Args) < 2 {
+		fmt.Println("no command provided")
+		os.Exit(2)
+	}
+	cmd := os.Args[1]
+	switch cmd {
+	case "greet":
+		msg := "CLI Basics"
+		if len(os.Args) > 2 {
+			f := strings.Split(os.Args[2], "=")
+			if len(f) == 2 && f[0] == "--msg" {
+				msg = f[1]
+			}
+		}
+		fmt.Printf("hello and welcome: %s\n", msg)
+	case "help":
+		fmt.Println("some help message")
+	default:
+		fmt.Printf("unknown command: %s\n", cmd)
+		os.Exit(2)
+	}
+}
